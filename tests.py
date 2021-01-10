aeb3f6fa1c359be0b79d8d30670e56e0ecfabc26
Zero <zeroto521@gmail.com>
2021-01-10 16:24:17 +0800

    Add language test

--- tests.py
+++ tests.py
@@ -4,18 +4,28 @@ import json
 # constants
 plugin_info_file = 'plugins.json'
 id_name = 'ID'
+language_name = 'Language'
+language_list = ('csharp', 'executable', 'fsharp', 'python')
+
 
 # read `plugin_info_file`
 with open(plugin_info_file) as f:
     plugin_infos = json.loads(f.read())
 
 
-def clean_uuid(old_id: str) -> str:
-    return old_id.lower().replace('-', '')
+def clean(old: str) -> str:
+    return old.lower().replace('-', '').strip()
 
 
 def test_uuid_unique():
     uuids = [plugin_info[id_name] for plugin_info in plugin_infos]
-    uuids = [clean_uuid(uuid) for uuid in uuids]
+    uuids = [clean(uuid) for uuid in uuids]
 
     assert len(uuids) == len(set(uuids))
+
+
+def test_language_in_list():
+    languages = [plugin_info[language_name] for plugin_info in plugin_infos]
+    languages = [clean(language) for language in languages]
+
+    assert set(language_list) >= set(languages)
