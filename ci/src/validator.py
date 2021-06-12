0762cc9cfd58d35f8edc00b2cad042e86fdc34f3
Zero <Zeroto521@gmail.com>
2021-06-12 08:55:33 +0800

    EHN: no fixed string

--- ci/src/validator.py
+++ ci/src/validator.py
@@ -8,7 +8,7 @@ def test_uuid_unique():
     uuids = [info[id_name] for info in plugin_infos]
     uuids = [clean(uuid) for uuid in uuids]
 
-    msg = "The 'ID' is not unique."
+    msg = f"The '{id_name}' is not unique."
     assert len(uuids) == len(set(uuids)), msg
 
 
@@ -16,5 +16,5 @@ def test_language_in_list():
     languages = [info[language_name] for info in plugin_infos]
     languages = [clean(language) for language in languages]
 
-    msg = f"The 'Language' is not in the list of {language_list}"
+    msg = f"The '{language_name}' is not in the list of {language_list}"
     assert set(language_list) >= set(languages), msg
