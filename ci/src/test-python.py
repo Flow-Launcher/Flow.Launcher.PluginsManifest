9a9196882fbfd469aafe59d3d5652a25168f5f2a
Garulf <535299+Garulf@users.noreply.github.com>
2021-11-16 15:33:58 -0500

    Method to run plugin

--- ci/src/test-python.py
+++ ci/src/test-python.py
@@ -38,6 +38,21 @@ def get_latest_plugin(manifest):
         sys.exit(1)
     return _plugin
 
+def run_plugin(plugin_path, execute_path):
+    os.chdir(plugin_path)
+    p = Popen(["python", "-S", Path(Path(plugin_path, execute_path)), '{\"method\": \"query\", \"parameters\": [\"\"]}'], text=True, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate()
+    exit_code = p.wait()
+    if stdout != "":
+        print(f'{"#" * 9} Output {"#" * 9}\n{stdout}')
+    if exit_code == 0:
+        print("Test passed!")
+    else:
+        print(f'Test failed!\nPlugin returned a non-zero exit code!\n{"#" * 9} Trace {"#" * 9}')
+        if stderr != "":
+            print(stderr)
+        sys.exit(exit_code)
+
 if __name__ == "__main__":
     manifest = plugin_reader()
     plugin = get_latest_plugin(manifest)
@@ -55,20 +70,8 @@ if __name__ == "__main__":
     for path in Path(zip_dir).glob("**/plugin.json"):
         execute_file = read_plugin(path)["ExecuteFileName"]
         plugin_path = Path(path).parent
-    os.chdir(plugin_path)
     print("Running plugin...")
-    p = Popen(["python", "-S", Path(Path(plugin_path, execute_file)), '{\"method\": \"query\", \"parameters\": [\"\"]}'], text=True, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = p.communicate()
-    exit_code = p.wait()
-    if stdout != "":
-        print(f'{"#" * 9} Output {"#" * 9}\n{stdout}')
-    if exit_code == 0:
-        print("Test passed!")
-    else:
-        print(f'Test failed!\nPlugin returned a non-zero exit code!\n{"#" * 9} Trace {"#" * 9}')
-        if stderr != "":
-            print(stderr)
-        sys.exit(exit_code)
+    run_plugin(plugin_path, execute_file)
 
 
 
