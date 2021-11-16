50d3180522e6ac96635043458683ffd019c4f857
Garulf <535299+Garulf@users.noreply.github.com>
2021-11-16 15:26:21 -0500

    Print steps to console

--- ci/src/test-python.py
+++ ci/src/test-python.py
@@ -41,24 +41,30 @@ def get_latest_plugin(manifest):
 if __name__ == "__main__":
     manifest = plugin_reader()
     plugin = get_latest_plugin(manifest)
+    print(f"Found untested plugin: {plugin['Name']}")
     latest_release = get_download_url(plugin["UrlSourceCode"])
+    print("Downloading latest release...")
     z = download_release(latest_release)
     zip_dir = Path.cwd().joinpath("plugin")
     try:
         os.mkdir(zip_dir)
     except FileExistsError:
         pass
+    print("Extracting...")
     z.extractall(zip_dir)
     for path in Path(zip_dir).glob("**/plugin.json"):
         execute_file = read_plugin(path)["ExecuteFileName"]
         plugin_path = Path(path).parent
     os.chdir(plugin_path)
+    print("Running plugin...")
     p = Popen(["python3", "-S", Path(Path(plugin_path, execute_file)), '{\"method\": \"query\", \"parameters\": [\"\"]}'], text=True, stdout=PIPE, stderr=PIPE)
     stdout, stderr = p.communicate()
     exit_code = p.wait()
     if stdout != "":
-        print(stdout)
-    if exit_code != 0:
+        print(f'{"#" * 9} Output {"#" * 9}\n{stdout}')
+    if exit_code == 0:
+        print("Test passed!")
+    else:
         print(f'Test failed!\nPlugin returned a non-zero exit code!\n{"#" * 9} Trace {"#" * 9}')
         if stderr != "":
             print(stderr)
