7300579f96e6b5391ae31fd3c37cd299fee9b9fa
Hongtao Zhang <hongtao_zhang@outlook.com>
2023-08-25 19:10:36 +0800

    use async io to improve performance

--- ci/src/updater.py
+++ ci/src/updater.py
@@ -1,18 +1,22 @@
 # -*-coding: utf-8 -*-
+import asyncio
 from http.client import responses
 from typing import List
 from unicodedata import name
 from sys import argv
 import traceback
+import aiohttp
 
 import requests
-from tqdm import tqdm
+from tqdm.asyncio import tqdm
 
 from _utils import *
 from discord import update_hook
 
 
-def batch_github_plugin_info(info: P, tags: ETagsType, webhook_url: str = None) -> P:
+async def batch_github_plugin_info(
+    info: P, tags: ETagsType, webhook_url: str = None
+) -> P:
     try:
         headers = None
         if "github.com" not in info[url_download]:
@@ -26,70 +30,92 @@ def batch_github_plugin_info(info: P, tags: ETagsType, webhook_url: str = None) 
         tag: str = tags.get(info[id_name], info.get(etag, ""))
 
         if release_date in info.keys():
-            headers = {"If-None-Match": tag}
-        res = requests.get(
-            url_release.format(repo=repo),
-            headers=headers,
-        )
-        if res.status_code in (403, 304):
-            return info
+            headers = {
+                "If-None-Match": tag,
+                "Authorization": "Bearer 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",
+            }
 
-        latest_rel = res.json()
-        assets = latest_rel.get("assets")
-        if info.get(release_date, '') != latest_rel.get('published_at'):
-            info[release_date] = latest_rel.get('published_at')
-        if assets:
-            info[url_download] = assets[0]["browser_download_url"]
-            send_notification(info, clean(
-                latest_rel["tag_name"], "v"), latest_rel, webhook_url)
-            info[version] = clean(latest_rel["tag_name"], "v")
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                url_release.format(repo=repo),
+                headers=headers,
+            ) as res:
+                if res.status in (403, 304):
+                    return info
 
-        tags[info[id_name]] = res.headers.get(etag, "")
+                latest_rel = await res.json()
 
-        return info
+                assets = latest_rel.get("assets")
+                if info.get(release_date, "") != latest_rel.get("published_at"):
+                    info[release_date] = latest_rel.get("published_at")
+                if assets:
+                    info[url_download] = assets[0]["browser_download_url"]
+                    await send_notification(
+                        info,
+                        clean(latest_rel["tag_name"], "v"),
+                        latest_rel,
+                        webhook_url,
+                    )
+                    info[version] = clean(latest_rel["tag_name"], "v")
+
+                tags[info[id_name]] = res.headers.get(etag, "")
+
+                return info
     except Exception as e:
         tb = traceback.format_exc()
         print(f"Error when processing plugin {info[plugin_name]}:\n{e} {tb}")
         return info
 
 
-def batch_plugin_infos(plugin_infos: Ps, tags: ETagsType, webhook_url: str = None) -> Ps:
-    return [batch_github_plugin_info(info, tags, webhook_url) for info in tqdm(plugin_infos)]
+async def batch_plugin_infos(
+    plugin_infos: Ps, tags: ETagsType, webhook_url: str = None
+) -> Ps:
+    return await tqdm.gather(
+        *[
+            batch_github_plugin_info(info, tags, webhook_url)
+            for info in tqdm(plugin_infos)
+        ]
+    )
 
 
 def remove_unused_etags(plugin_infos: Ps, etags: ETagsType) -> ETagsType:
     etags_updated = {}
     plugin_ids = [info.get("ID") for info in plugin_infos]
-    
+
     for id, tag in etags.items():
-        
         if id not in plugin_ids:
-            print(f"Plugin with ID {id} has been removed. The associated ETag will be also removed now.")
+            print(
+                f"Plugin with ID {id} has been removed. The associated ETag will be also removed now."
+            )
             continue
-        
+
         etags_updated[id] = tag
-    
+
     return etags_updated
 
 
-def send_notification(info: P, latest_ver, release, webhook_url: str = None) -> None:
+async def send_notification(info: P, latest_ver, release, webhook_url: str = None) -> None:
     if version_tuple(info[version]) != version_tuple(latest_ver):
         tqdm.write(f"Update detected: {info[plugin_name]} {latest_ver}")
         try:
-            update_hook(webhook_url, info, latest_ver, release)
+            await update_hook(webhook_url, info, latest_ver, release)
         except Exception as e:
-            tqdm.write(e)
+            tqdm.write(str(e))
 
 
-if __name__ == "__main__":
+async def main():
     webhook_url = None
     if len(argv) > 1:
         webhook_url = argv[1]
     plugin_infos = plugin_reader()
     etags = etag_reader()
-    
-    plugin_infos_new = batch_plugin_infos(plugin_infos, etags, webhook_url)
+
+    plugin_infos_new = await batch_plugin_infos(plugin_infos, etags, webhook_url)
     plugin_writer(plugin_infos_new)
-    
+
     etags_new = remove_unused_etags(plugin_infos_new, etags)
     etags_writer(etags_new)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
