10f36f68924537cb88bf47034533495f9c778409
Kevin Zhang <45326534+taooceros@users.noreply.github.com>
2024-07-07 10:11:47 -0700

    fix merge manifest and use glob to enumerate files (#359)
    * fix merge manifest and use glob to enumerate files
    - also remove duplicate manifest
    
    * change filter
    
    * fix again
    
    * fix skip duplicate action
    
    * fix some escape issue
    
    * fix encoding
    
    * fix encoding

--- ci/src/merge-manifest.py
+++ ci/src/merge-manifest.py
@@ -1,17 +1,15 @@
-import os
-from os import path
+import glob
 import json
 
 if __name__ == "__main__":
-    plugins = [file for file in os.listdir("plugins") if path.isfile(file) and file.endswith("json")]
+    plugins = glob.glob("plugins/*.json")
 
     manifests = []
 
     for plugin in plugins:
-        with open(plugin, "r") as f:
+        with open(plugin, "r", encoding="utf-8") as f:
             manifest = json.load(f)
             manifests.append(manifest)
 
-    with open("plugins.json", "w") as f:
-        json.dump(manifests, f, indent=4)
-        
+    with open("plugins.json", "w", encoding="utf-8") as f:
+        json.dump(manifests, f, indent=4, ensure_ascii=False)
