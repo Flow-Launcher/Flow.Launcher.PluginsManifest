e3a8db991f38f8a359f5545c23b0ca1e0a13e41d
cibere <71997063+cibere@users.noreply.github.com>
2025-02-04 08:42:45 -0800

    format update-tested + remove unused imports

--- ci/src/update-tested.py
+++ ci/src/update-tested.py
@@ -1,14 +1,10 @@
-import sys
-import json
-import os
-import zipfile
-import io, asyncio
+import asyncio
 from datetime import datetime, UTC
 from sys import argv
 from os import getenv
-from _utils import clean, id_name, language_list, version, plugin_reader, plugin_writer, release_date, date_added, etag_reader, PluginType, ETagsType
+from _utils import plugin_reader, plugin_writer, date_added, etag_reader
 from updater import batch_github_plugin_info
-from discord import release_hook
+
 
 async def update_tested():
     webhook_url = None
@@ -24,15 +20,15 @@ async def update_tested():
             plugin_infos[idx]["Tested"] = True
         # Add date added if field is not present
         if plugin.get(date_added) is None:
-            plugin_infos[idx][date_added] = datetime.now(UTC).strftime("%Y-%m-%dT%H:%M:%SZ")
-            await batch_github_plugin_info(plugin, etags, github_token, webhook_url, True)
-    
+            plugin_infos[idx][date_added] = datetime.now(UTC).strftime(
+                "%Y-%m-%dT%H:%M:%SZ"
+            )
+            await batch_github_plugin_info(
+                plugin, etags, github_token, webhook_url, True
+            )
+
     plugin_writer(plugin_infos)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(update_tested())
-
-
-
-
-
